1fcab1744183b51df8d881ab1f5f8eb6af9ba40a
gurjnderSingh <gurjindersingh663@gmail.com>
2023-11-03 22:52:39 +0530

    Api call added

--- ApiDemo/ApiDemo/ContentView.swift
+++ ApiDemo/ApiDemo/ContentView.swift
@@ -7,15 +7,61 @@
 
 import SwiftUI
 
+struct Info: Codable {
+//    var id = UUID().uuidString
+    var users: [Users]
+    var total, skip, limit: Int
+}
+
+struct Users: Codable {
+    var id: Int
+    var firstName, lastName, maidenName, gender, email, image: String
+}
+
 struct ContentView: View {
+    
+    @State private var info = [Users]()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+
+            List(info, id: \.id) { info in
+                VStack(alignment: .leading) {
+                    Text(info.firstName)
+                    Text(info.lastName)
+                    Text(info.maidenName)
+                    Text(info.email)
+                    Text(info.gender)
+                    Text(info.image)
+                }
+            }
+            .task {
+                await fetchData()
+            }
         }
-        .padding()
+        .navigationTitle("Breaking Bad")
+    }
+    
+    // create URL and fetch data
+    
+    func fetchData() async {
+        guard let url = URL(string: "https://dummyjson.com/users") else {
+            print("Bad URL")
+            return
+        }
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url)
+            // decode data
+            print("Data received", data)
+            if let decodedRes = try? JSONDecoder().decode(Info.self, from: data) {
+                print(decodedRes)
+                self.info = decodedRes.users
+            } else {
+                print("failed")
+            }
+        } catch {
+            print("failed to fetch request")
+        }
     }
 }
 
