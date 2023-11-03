1cd154a202f4b75ef7477bf1c76023446897fd26
gurjnderSingh <gurjindersingh663@gmail.com>
2023-11-03 22:10:44 +0530

    inital commit

---
+++ ApiDemo/ApiDemo/ApiDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ApiDemoApp.swift
+//  ApiDemo
+//
+//  Created by Gurjinder Singh on 03/11/23.
+//
+
+import SwiftUI
+
+@main
+struct ApiDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
